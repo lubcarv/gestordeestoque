09ea8834f029b2c439e4284842b2218c12e9d0f0
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-10-30 16:51:44 -0300

    Deploy no render4

--- gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/GestordeestoqueApplication.java
+++ gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/GestordeestoqueApplication.java
@@ -2,10 +2,8 @@ package com.luizabau.gestordeestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
-@EnableJdbcHttpSession
 public class GestordeestoqueApplication {
 
 	public static void main (String[] args) {
