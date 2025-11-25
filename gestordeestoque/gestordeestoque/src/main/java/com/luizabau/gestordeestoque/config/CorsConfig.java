c59b09bb70e0f1d78a411589da73748a8012d0d4
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-11-24 21:06:19 -0300

    Merge remote-tracking branch 'origin/master'

--- gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/CorsConfig.java
+++ gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/CorsConfig.java
@@ -9,9 +9,14 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "https://gestordeestoquefront.onrender.com",
+                        "http://localhost:3000",
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true);
     }
 }
