9b0b6c947118f1cdb261029afbdde1807cabee0d
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-11-26 17:19:55 -0300

    Merge remote-tracking branch 'origin/master'

--- gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/domain/MovimentacaoEstoque.java
+++ gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/domain/MovimentacaoEstoque.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "MovimentacaoEstoque")
+@Table(name = "movimentacaoestoque")
 public class MovimentacaoEstoque {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
