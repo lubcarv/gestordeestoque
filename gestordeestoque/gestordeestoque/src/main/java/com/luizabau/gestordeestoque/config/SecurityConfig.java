28f8c5127aed7868fbed5edd094834697cb74831
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-12-02 13:34:47 -0300

    Update SecurityConfig.java

--- gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/SecurityConfig.java
+++ gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -13,11 +14,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+                .authorizeHttpRequests(auth -> auth. anyRequest().permitAll())
+                . formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .cors(cors -> cors.disable());
         return http.build();
     }
 }
