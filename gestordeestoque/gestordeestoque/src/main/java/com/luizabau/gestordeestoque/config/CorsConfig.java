9f6663a6c9da6f618ec3d8fc0138b93fdb3cf0aa
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-12-02 12:59:43 -0300

    Update CorsConfig.java

--- gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/CorsConfig.java
+++ gestordeestoque/gestordeestoque/src/main/java/com/luizabau/gestordeestoque/config/CorsConfig.java
@@ -4,33 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins()
-////                .allowedOrigins(
-////                        "https://gestordeestoquefront.onrender.com",
-////                        "http://localhost:3000",
-////                        "http://localhost:5500",
-////                        "http://127.0.0.1:5500",
-////                        "http://localhost:8081",
-////                        "http://127.0.0.1:8081"
-////                )
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                . allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(
+                    "https://gestordeestoquefront.onrender.com",
+                    "http://localhost:3000",
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5500",
+                    "http://localhost:8081",
+                    "http://127.0.0.1:8081"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
